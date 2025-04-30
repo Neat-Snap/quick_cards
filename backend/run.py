49ca62cb90e4ca6d61e45b41c213cc5fded0f7a2
Spikker <matt.bogomolov@gmail.com>
2025-04-30 21:58:55 +0300

    Test: implementing payments 8

--- backend/run.py
+++ backend/run.py
@@ -9,6 +9,8 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.main import app
+from app.bot import bot
 
 if __name__ == "__main__":
+    bot.infinity_polling()
     app.run(host="0.0.0.0", port=8000, debug=True)
