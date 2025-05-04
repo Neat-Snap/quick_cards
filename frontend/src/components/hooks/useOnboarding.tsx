8afdca5d7715de20f5efa59e3b9ca43b6e8d1cb3
Spikker <matt.bogomolov@gmail.com>
2025-05-04 23:23:02 +0300

    Onboarding 9

--- frontend/src/components/hooks/useOnboarding.tsx
+++ frontend/src/components/hooks/useOnboarding.tsx
@@ -16,7 +16,7 @@ export function useOnboarding() {
       // This relies on your API returning an is_new_user flag
       console.log("is showing onboarding", user.name === "")
       console.log("name is here", user.name)
-      setShowOnboarding(user.name === "");
+      setShowOnboarding(!user.name);
       setIsLoading(false);
     } else {
       console.log("NOT USER")
